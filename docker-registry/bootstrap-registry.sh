a74572a51be4bd8f772f03e371f17f54091c704c
Sergii Mikhtoniuk <mikhtonyuk@gmail.com>
2014-01-27 16:44:20 +0200

    fixing upstart

--- docker-registry/bootstrap-registry.sh
+++ docker-registry/bootstrap-registry.sh
@@ -11,6 +11,9 @@ sudo apt-get install -qy build-essential python-dev libevent-dev python-pip libs
 sudo pip install -r requirements.txt
 
 cp -f config/config_sample.yml config/config.yml
+sudo sed -i -e 's/storage_path: \/tmp\/registry/storage_path: \/home\/vagrant\/.registry/g' config/config.yml
+
+mkdir -p /home/vagrant/.registry
 mkdir -p /var/log/docker-registry
 
 sudo cat << EOF > /etc/init/docker-registry.conf
@@ -20,15 +23,17 @@ author "Docker, Inc."
 
 start on runlevel [2345]
 stop on runlevel [016]
-respawn respawn limit 10 5
+
+respawn
+respawn limit 10 5
 
 # set environment variables
-env REGISTRY_HOME=/opt/docker-registry
-env SETTINGS_FLAVOR=prod
+env REGISTRY_HOME=/home/vagrant/docker-registry
+env SETTINGS_FLAVOR=dev
 
 script
-	cd $REGISTRY_HOME
-	exec gunicorn -k gevent --max-requests 100 --graceful-timeout 3600 -t 3600 -b 0.0.0.0:5000 -w 8 --access-logfile /var/log/docker-registry/access.log --error-logfile /var/log/docker-registry/error.log wsgi:application
+	cd \$REGISTRY_HOME
+	exec gunicorn -k gevent --max-requests 100 --graceful-timeout 3600 -t 3600 -b 0.0.0.0:5000 -w 1 --access-logfile /var/log/docker-registry/access.log --error-logfile /var/log/docker-registry/error.log wsgi:application
 end script
 EOF
 
